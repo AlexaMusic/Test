5c4996107b924ce45cc13ea30584a2fce0c9d7ec
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-03-09 05:07:09 +0500

    g

--- TikTok.py
+++ TikTok.py
@@ -685,46 +685,52 @@ async def alexaprivate(client: Client, message: Message):
 
 
 @bot.on_message(
- (
-        filters.sticker
-        | filters.text
-    )
+    (filters.sticker | filters.text)
     & filters.private
     & ~filters.bot,
 )
 async def alexaprivatesticker(client: Client, message: Message):
 
-   chatdb = MongoClient(MONGO_URL)
-   chatai = chatdb["Word"]["WordDb"] 
-   if not message.reply_to_message:
-       await bot.send_chat_action(message.chat.id, "typing")
-       K = []  
-       is_chat = chatai.find({"word": message.sticker.file_unique_id})                 
-       for x in is_chat:
-           K.append(x['text'])
-       hey = random.choice(K)
-       is_text = chatai.find_one({"text": hey})
-       Yo = is_text['check']
-       if Yo == "text":
-           await message.reply_text(f"{hey}")
-       if not Yo == "text":
-           await message.reply_sticker(f"{hey}")
-   if message.reply_to_message:            
-       getme = await bot.get_me()
-       bot_id = getme.id       
-       if message.reply_to_message.from_user.id == bot_id:                    
-           await bot.send_chat_action(message.chat.id, "typing")
-           K = []  
-           is_chat = chatai.find({"word": message.sticker.file_unique_id})                 
-           for x in is_chat:
-               K.append(x['text'])
-           hey = random.choice(K)
-           is_text = chatai.find_one({"text": hey})
-           Yo = is_text['check']
-           if Yo == "text":
-               await message.reply_text(f"{hey}")
-           if not Yo == "text":
-               await message.reply_sticker(f"{hey}")
+    chatdb = MongoClient(MONGO_URL)
+    chatai = chatdb["Word"]["WordDb"] 
+    if not message.reply_to_message:
+        await bot.send_chat_action(message.chat.id, "typing")
+        K = []  
+        is_chat = chatai.find({"word": message.sticker.file_unique_id})                 
+        for x in is_chat:
+            K.append(x['text'])
+        hey = random.choice(K)
+        is_text = chatai.find_one({"text": hey})
+        Yo = is_text['check']
+        if Yo == "text":
+            await message.reply_text(f"{hey}")
+        if not Yo == "text":
+            gif = is_text['gif']
+            await message.reply_animation(gif)
+    if message.reply_to_message:            
+        getme = await bot.get_me()
+        bot_id = getme.id       
+        if message.reply_to_message.from_user.id == bot_id:                    
+            await bot.send_chat_action(message.chat.id, "typing")
+            K = []  
+            is_chat = chatai.find({"word": message.sticker.file_unique_id})                 
+            for x in is_chat:
+                K.append(x['text'])
+            hey = random.choice(K)
+            is_text = chatai.find_one({"text": hey})
+            Yo = is_text['check']
+            if Yo == "text":
+                await message.reply_text(f"{hey}")
+            if not Yo == "text":
+                gif = is_text['gif']
+                await message.reply_animation(gif)
+        else:
+            if message.sticker:
+                file_unique_id = message.sticker.file_unique_id
+                if chatai.find({"word": file_unique_id}).count() == 0:
+                    gif = message.sticker.file_id
+                    chatai.insert_one({"word": file_unique_id, "gif": gif})
+
 
        
 bot.run()
