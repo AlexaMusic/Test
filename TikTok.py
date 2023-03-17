18a444d7c7b09403b6a1565d6319364690b4355b
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-03-18 03:03:42 +0500

    g

--- TikTok.py
+++ TikTok.py
@@ -59,11 +59,6 @@ async def handle_message(client: userbot, message: Message):
     user_msg = message.text
     user_unme = message.from_user.username
     await userbot.send_message(LOG_GROUP, f"{sender_name} sent a message with username @{user_unme}: {user_msg}")
-    text = message.text.lower()
-    if any(word in text for word in JHANTO_LOG_WORD):
-        await userbot.block_user(user_id)
-        await message.reply("You have been blocked for using inappropriate language.")
-        return
     increment_user_message_count(user_id)
     message_count = get_user_message_count(user_id)
     if not is_approved(user_id) and message_count >= WARNING_LIMIT:
@@ -71,7 +66,12 @@ async def handle_message(client: userbot, message: Message):
         if message_count >= WARNING_LIMIT + 1:
             await userbot.block_user(user_id)
             await message.reply("You have been blocked for sending too many messages.")
-
+        return
+    text = message.text.lower()
+    if any(word in text for word in JHANTO_LOG_WORD):
+        await userbot.block_user(user_id)
+        await message.reply("You have been blocked for using inappropriate language.")
+        
 @userbot.on_message(
     filters.command(["a", "approve"], CMD_HANDLER) & filters.me & filters.private
 )
