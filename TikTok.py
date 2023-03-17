0d202a1e4efb668842df5197f8fd44678fcb6dfc
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-03-18 03:14:07 +0500

    gh

--- TikTok.py
+++ TikTok.py
@@ -58,19 +58,22 @@ async def handle_message(client: userbot, message: Message):
     sender_name = message.from_user.first_name
     user_msg = message.text
     user_unme = message.from_user.username
-    await userbot.send_message(LOG_GROUP, f"{sender_name} sent a message with username @{user_unme}: {user_msg}")
-    increment_user_message_count(user_id)
-    message_count = get_user_message_count(user_id)
-    if not is_approved(user_id) and message_count >= WARNING_LIMIT:
-        await message.reply(f"Warning! You are not an approved user so you have a limitation on the number of messages you can send. You have sent {message_count} messages. You will be blocked after {WARNING_LIMIT} messages.")
-        if message_count >= WARNING_LIMIT + 1:
-            await userbot.block_user(user_id)
-            await message.reply("You have been blocked for sending too many messages.")
-            return
+    await userbot.send_message(LOG_GROUP, f"{sender_name} send a message it's username @{user_unme}: {user_msg}")
     text = message.text.lower()
     if any(word in text for word in JHANTO_LOG_WORD):
         await userbot.block_user(user_id)
         await message.reply("You have been blocked for using inappropriate language.")
+        return
+    increment_user_message_count(user_id)
+    message_count = get_user_message_count(user_id)
+    if message_count >= WARNING_LIMIT:
+        await message.reply(f"Warning! You have sent {message_count} messages. You will be blocked after {WARNING_LIMIT} messages.")
+    if message_count >= WARNING_LIMIT + 1:
+        await userbot.block_user(user_id)
+        await message.reply("You have been blocked for sending too many messages.")
+        return
+    if not is_approved(user_id):
+        await message.reply("You are not an approved user.")
         
 @userbot.on_message(
     filters.command(["a", "approve"], CMD_HANDLER) & filters.me & filters.private
