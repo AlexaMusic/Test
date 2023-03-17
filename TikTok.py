506694ce9879bd8330d7151d31eb5b6ee9ea95b2
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-03-18 04:07:45 +0500

    ck

--- TikTok.py
+++ TikTok.py
@@ -49,12 +49,6 @@ def increment_user_message_count(user_id):
         user_document = {"user_id": user_id, "message_count": 0}
     user_document["message_count"] += 1
     approved_users_collection.replace_one({"user_id": user_id}, user_document)
-    
-def reset_user_message_count(user_id):
-    user_document = approved_users_collection.find_one({"user_id": user_id})
-    if user_document is not None:
-        user_document["message_count"] = 0
-        approved_users_collection.replace_one({"user_id": user_id}, user_document)
 
 @userbot.on_message(
     ~filters.me & filters.private & ~filters.bot & filters.incoming, group=69
@@ -64,24 +58,22 @@ async def handle_message(client: userbot, message: Message):
     sender_name = message.from_user.first_name
     user_msg = message.text
     user_unme = message.from_user.username
-    await userbot.send_message(LOG_GROUP, f"{sender_name} send a message it's username @{user_unme}: {user_msg}")
+    await userbot.send_message(LOG_GROUP, f"{sender_name} sent a message, username: @{user_unme}, message: {user_msg}")
     text = message.text.lower()
     if any(word in text for word in JHANTO_LOG_WORD):
         await userbot.block_user(user_id)
         await message.reply("You have been blocked for using inappropriate language.")
         return
-    await increment_user_message_count(user_id)
-    message_count = await get_user_message_count(user_id)
-    approved = await is_approved(user_id)
-    if not approved:
-        if message_count >= WARNING_LIMIT:
+    increment_user_message_count(user_id)
+    message_count = get_user_message_count(user_id)
+    if message_count >= WARNING_LIMIT:
+        if is_approved(user_id):
+            reset_user_message_count(user_id)
+        else:
             await message.reply(f"Warning! You are not an approved user so you have a limitation on the number of messages you can send. You have sent {message_count} messages. You will be blocked after {WARNING_LIMIT} messages.")
-        if message_count >= WARNING_LIMIT + 1:
-            await userbot.block_user(user_id)
-            await message.reply("You have been blocked for sending too many messages.")
-            return
-    else:
-        reset_user_message_count(user_id)
+    if message_count >= WARNING_LIMIT + 1:
+        await userbot.block_user(user_id)
+        await message.reply("You have been blocked for sending too many messages.")
         
 @userbot.on_message(
     filters.command(["a", "approve"], CMD_HANDLER) & filters.me & filters.private
